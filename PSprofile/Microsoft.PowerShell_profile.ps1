003c5cfadd427e84aecc2388aa0a8ad644c38e04
PSamardzhiev <petko9001@gmail.com>
2024-04-08 23:19:16 +0300

    Updated PS Profile script to determine if the current PS session is exectued with administrator privileges

--- PSprofile/Microsoft.PowerShell_profile.ps1
+++ PSprofile/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,21 @@
 Clear-Host
 $aliasKeys = @("v", "t", "k", "vs", "py", "rst", "hvm")
 $banner_text = "Welecome to your custom Windows Terminal Environment"
+function Test-AdministratorPrivileges {
+    $isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+    if ($isAdmin) {
+        Write-Host "This PowerShell session is running with Administrator privileges." -BackgroundColor Black -ForegroundColor Red
+    } else {
+        Write-Host "This PowerShell session is NOT running with Administrator privileges." -BackgroundColor Black -ForegroundColor Green
+    }
+}
+
+
 Write-Host $banner_text `n"Your data is located at: "$env:OneDriveConsumer -BackgroundColor Black -ForegroundColor Green
 Write-Host "Logged-in username: $env:USERNAME" -BackgroundColor Black -ForegroundColor Green
 Write-Host "Your computername: $env:COMPUTERNAME" -BackgroundColor Black -ForegroundColor Green
 Write-Host "Some of your custom terminal functions: `n $aliasKeys"
+Test-AdministratorPrivileges
 
 #load starship config
 Invoke-Expression $(starship init powershell)
